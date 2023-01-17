8aab5d78576a859203d8465d36eec9b294ba3350
AlexanderCaballero05 <aacaballero@unah.hn>
2023-01-17 12:11:03 -0600

    Cambios 17/01/23

---
+++ menu.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PANI-Instituto Nacional de la Infancia</title>
+    <link rel="icon" href="img/pani1.png" type="image/png">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="css/style.css" rel="stylesheet"/>
+    
+</head>
+<body>
+         
+  <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top p-2">
+    <div class="container-fluid">
+   <a class="navbar-brand" href="" style="background-color: transparent !important;"><!--Referencia del logo de la empresa-->
+    <img src="img/pani1.png" class="margin d-block" alt="logo-pani" width="160px" style="">
+  </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Sorteos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Premios</a>
+          <li class="nav-item">
+              <a class="nav-link" href="noticias.php">Noticias</a>
+            </li>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="institucion.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Institución</a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="institucion.php">Historia</a></li>
+              <li><a class="dropdown-item" href="institucion.php">Misión/Visión </a></li>
+            <li><a class="dropdown-item" href="institucion.php">Valores </a></li>
+
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Productos</a></li>
+          </ul>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="contacto.php">Contacto</a>
+        </li>
+      </ul>
+      <!--
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-primary" type="submit" >Buscar</button>
+      </form>
+-->
+    </div>
+  </div>
+</nav>
+</body>
+</html>
---
+++ noticias.php
@@ -0,0 +1,183 @@
+<!--consulta php-->
+<?php  
+ include_once "conexion.php";
+
+ $query_titulo = "SELECT TITULO FROM TBL_NOTICIAS";
+ $result_titulo =$conn->query($query_titulo);
+
+ $query = "SELECT * FROM TBL_NOTICIAS";
+ $result_query =$conn->query($query);
+?>
+<!--fin consulta php-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PANI-Patronato Nacional de la Infancia</title>
+    <link rel="icon" href="img/pani1.png" type="image/png">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="css/style.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Raleway:wght@600&family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+<style>
+
+html, body, * {
+    padding: 0;
+    margin: 0;
+}
+html {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+}
+
+#left1 { /*Estilos del left fix */
+  margin-top: 70px;
+    background: rgb(216 220 225 / 26%);;
+    position: fixed;
+    left: -15px;
+    top: 0;
+    bottom: 0;
+    right: 78%;
+    overflow-x:  scroll;
+    padding: 2em;
+}
+#right1 { /*Estilos del right scrolling */
+  margin-top: 70px;
+    background:#fdfbfb ;
+    position: absolute;
+    left: 25%;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    /*overflow-y: auto; */
+    padding: 2.5em;
+  min-height:1000px;
+}
+
+</style>
+</head>
+<body>
+    
+    <!---------------------------------------INICIO DE MENU--------------------------------------->
+  <?php include "menu.php";?>
+  
+    <!---------------------------------------CIERRE DE MENU--------------------------------------->
+  
+<!-------------------------------Inicio de seccion de Noticias------------------------------->
+  
+
+  <div id="container"><!--Container-->
+    <div id="left1">&nbsp;<!--Div izquierdo left fix-->
+            <h3 class="text-center">Menu</h3>
+        <nav class="nav">
+           <ul class="list">
+             <li class="list__item">
+                <div class="list__button">
+                    <img src="assets/dashboard.svg" class="list__img">
+                    <a href="#" class="nav__link">Inicio</a>
+                </div>
+             </li>
+
+              <li class="list__item list__item--click">
+                <div class="list__button list__button--click">
+                    <img src="assets/docs.svg" class="list__img">
+                    <a href="#" class="nav__link">Titulos de Noticias</a>
+                    <img src="assets/arrow.svg" class="list__arrow">
+                </div>
+
+                <ul class="list__show">
+                        <!--PHP Traer los titulos desde la Base de datos-->
+                        <?php 
+                            if($result_titulo->num_rows > 0){
+                              while($row = $result_titulo->fetch_array()){
+                                $titulos = $row['TITULO'];
+                
+                        ?>
+                        <!--FIN PHP Traer los titulos desde la Base de datos-->
+                     <li class="list__inside" id="listFocus">
+                        <a href="#" class="nav__link nav__link--inside"><?php echo $titulos?></a>
+                     </li>
+                       <!--PHP Traer los titulos desde la Base de datos-->
+                        <?php
+                               }
+                             }
+                        ?>
+                        <!--FIN PHP Traer los titulos desde la Base de datos-->
+                </ul>
+             </li>
+          </ul>
+        </nav>
+                     <!--Banner de noticias-->
+                     <div class="mx-auto" style="margin-top: 50px;">
+                     <a href="https://www.salud.gob.hn/site/index.php/covid19" target="_blank" style="background-color: transparent !important;"><button type="button" class="btn btn-danger w-100" style="padding: 12px !important;">Seguimiento covid-19 Honduras</button></a>
+                     </div>
+
+                <div class="mx-auto" style="margin-top: 50px; margin-left: 60px !important;">
+                   <a href="https://portalunico.iaip.gob.hn/#/homeLanding" target="_blank" style="background-color: transparent !important;"><img width="139" height="174" src="img/portal.png"></a>
+                </div><!--Banner de noticias-->
+                
+    </div><!--fin de Div izquierdo-->
+
+      <div id="right1">&nbsp;<!--Inicio Div right scrolling-->
+      <h1><strong>Sección de Noticias Pani</strong></h1>
+      <hr width="800" style="margin-bottom: 70px;">
+         <div class="row"><!--Row-->
+            <div class="col-sm-12"><!--column-->
+                <!--PHP ARRAYS para traer los datos de las noticias-->
+                        <?php 
+                            if($result_query->num_rows > 0){
+                              while($row = $result_query->fetch_array()){
+                                $titulo = $row['TITULO'];
+                                $cuerpo = $row['CUERPO'];
+                                $fecha =  $row['FECHA'];
+                                $image =  $row['IMAGEN'];
+                
+                        ?>
+                        <!--PHP ARRAYS para traer los datos de las noticias-->
+                  <article><!--Etiqueta articulo donde se colocaran las noticias-->
+                      <header class="post-info"><!--Titulo de la noticia-->
+                          <h2 class="post-title titulo" id="titleFocus"><?php echo $titulo; ?></h2>
+                        <ul class="info">
+                          <li><?php echo $fecha; ?></li>
+                          <li>Tegucigalpa</li>
+                        </ul>
+                      </header><!--Fin de Titulo de la noticia-->
+                        <div><!--cuerpo de la noticia-->
+                            <capture>
+                              <img style="border-radius: 6px; box-shadow: 0 0 10px 0 rgb(0 0 0 / 10%);" width="800" height="400" src="data:image/jpeg;base64,<?php echo  base64_encode($image); ?>">
+                            </capture>
+                          <P style=" padding: 25px 0px 25px; max-width: 800px;"><?php echo $cuerpo; ?></P>
+                        </div><!--Fin del cuerpo de la noticia-->
+                  </article><!--Fin de Etiqueta articulo donde se colocaran las noticias-->
+                    <hr width="800">
+                    <br>
+                        <!--PHP-->
+                          <?php
+                                  }
+                                }
+                          ?>
+                        <!--FIN PHP ARRAYS-->
+            </div><!--column-->
+        </div><!--Row-->
+      </div><!--Fin Div derecho-->
+  </div><!--Fin de Container-->
+
+<!-------------------------------Fin de seccion de Noticias------------------------------->
+
+       
+
+
+            <script src="js/jquery-3.5.1.slim.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/main.js"></script>
+
+</body>
+</html>
