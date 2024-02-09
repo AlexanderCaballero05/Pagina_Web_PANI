e4bc6b50ee4f197a1d6898e1a585ac5dc4ff6aa0
AlexanderCaballero05 <aacaballero@unah.hn>
2024-02-09 16:44:37 -0600

    desarrollo de la plantilla HTML de la vista, codigo estatico

---
+++ Consumir-APIS/API-Administrador-web-site.js
@@ -0,0 +1,39 @@
+// const servicios = [
+      
+//     { id: 1,
+//       title: 'Vender loteria mayor',
+//       tipoServicio: 'Loteria Mayor',
+//       descripcion: 'Informacion sobre los requisitos de venta',
+//       imagen: './template/img/SIELHO.png'
+//     },
+
+//     { 
+//       id: 2,
+//       title: 'Vender loteria menor',
+//       tipoServicio: 'Loteria Menor',
+//       descripcion: 'Informacion sobre los requisitos de venta',
+//       imagen: './template/img/SIELHO.png'
+//     }
+//     ,
+//     {
+//       id: 3,
+//       title: 'Pago de premios loteria mayor',
+//       tipoServicio: 'Loteria Mayor',
+//       descripcion: 'Informacion sobre el pago de premios loteria mayor',
+//       imagen: './template/img/SIELHO.png'
+//     },
+//     ,
+//     {
+//       id: 3,
+//       title: 'Pago de premios loteria mayor',
+//       tipoServicio: 'Loteria Mayor',
+//       descripcion: 'Informacion sobre el pago de premios loteria mayor',
+//       imagen: './template/img/SIELHO.png'
+//     }
+  
+//   ];
+
+//   let DataJson = JSON.stringify(servicios);
+
+//  return DataJson;
+
